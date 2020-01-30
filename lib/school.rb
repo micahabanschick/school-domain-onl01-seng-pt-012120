3880565f8bc9c7ad4c046a6cdc630d10ae3cbfce
Micah Banschick <micha.ban@gmail.com>
2020-01-30 17:43:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  
+  attr_accessor :student 
+  
+  def initialize(school)
+    @school = school
+  end 
+  
+  def roster
+    roster
+  end
+    
+  
+  def add_student(student,grade)
+    self.roster[grade] = student 
+  end
+  
+  def grade
+  end 
+  
+  def sort 
+  end 
+  
+end
