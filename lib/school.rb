e33005162f7f5afd5735faca6e669ba691f6dbfb
Micah Banschick <micha.ban@gmail.com>
2020-01-30 18:36:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -23,7 +23,7 @@ class School
   end 
   
   def sort 
-    self.roster.sort{|grade1, grade2| grade1<=>grade2}.to_h
+    self.roster.values.sort{|grade1, grade2| grade1<=>grade2}.to_h
     
   end 
   
